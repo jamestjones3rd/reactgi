233aa6f3e4ad00f71f306a6b72f1560d50a31733
James Jones <jamesjones@Administrators-MacBook-Pro.local>
2023-07-10 14:12:49 -0400

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,125 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+// Very Easy
+// class App extends React.Component {
+// render() {
+// return (
+// <div>
+// <h1>Employee's information</h1>
+// <p>Name:James</p>
+// <p>Number:444444444</p>
+// <p>Date of Birth: 04/44/2044/p>
+// </div>
+// );
+// }
+// }
+
+
+
+
+
+
+// Easy/Medium
+// class App extends React.Component {
+// constructor(props) {
+// super(props);
+
+
+// this.state = {
+// person: {
+// name: " James",
+// number: "444444444",
+// DOB: "04/44/2044",
+// },
+// };
+// }
+// render() {
+// return <IndividualsInfo person={this.state.person} />;
+// }
+// }
+
+
+// class IndividualsInfo extends React.component {
+// render() {
+// const { person } = this.props;
+
+
+// return (
+// <div>
+// <h1>Employee's information</h1>
+// <p>Name:{person.name}</p>
+// <p>Number:{person.number}</p>
+// <p>Date of Birth: {person.DOB}</p>
+// </div>
+// );
+// }
+// }
+
+
+
+
+// Hard
+
+
+class App extends React.Component {
+constructor(props) {
+super(props);
+this.state = {
+people: [
+{
+name: 'James',
+number: '4444444444',
+DOB: '04/44/2044'
+},
+{
+name: 'Goatsiel',
+number: '4444444444',
+DOB: '07/27/2002'
+},
+{
+  name: 'James',
+  number: '4444444444',
+  DOB: '04/44/2044'
+},
+{
+  name: 'James',
+  number: '4444444444',
+  DOB: '04/44/2044'
 }
+]
+};
+}
+
+
+render() {
+const { people } = this.state;
+const employeeData = [];
+
+
+for (let i = 0; i < people.length; i++) {
+const person = people[i];
+employeeData.push(
+<div key={i}>
+<h2>Name: {person.name}</h2>
+<p>Number: {person.number}</p>
+<p>Date of Birth: {person.DOB}</p>
+</div>
+);
+}
+
+
+return (
+<div class="background">
+<h1>Employee's information</h1>
+{employeeData}
+</div>
+);
+}
+}
+
+
+
 
 export default App;
